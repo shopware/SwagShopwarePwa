15b513b6d8a08c220b86ff52705dbd905d0ea51d
Dominic Klein <d.klein@shopware.com>
2019-11-22 14:15:29 +0100

    WIP - Filters and sortings in page endpoint

--- src/VueStorefront/PageResult/Navigation/NavigationPageResultHydrator.php
+++ src/VueStorefront/PageResult/Navigation/NavigationPageResultHydrator.php
@@ -3,7 +3,15 @@
 namespace SwagVueStorefront\VueStorefront\PageResult\Navigation;
 
 use Shopware\Core\Content\Category\CategoryEntity;
+use Shopware\Core\Content\Cms\Aggregate\CmsSlot\CmsSlotEntity;
 use Shopware\Core\Content\Cms\CmsPageEntity;
+use Shopware\Core\Content\Product\SalesChannel\Listing\ProductListingResult;
+use Shopware\Core\Framework\DataAbstractionLayer\Entity;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\AggregationResult\Bucket\Bucket;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\AggregationResult\Bucket\TermsResult;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\AggregationResult\Metric\EntityResult;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\AggregationResult\Metric\MaxResult;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\AggregationResult\Metric\StatsResult;
 use Shopware\Storefront\Framework\Routing\Router;
 use SwagVueStorefront\VueStorefront\Controller\PageController;
 use SwagVueStorefront\VueStorefront\PageLoader\Context\PageLoaderContext;
@@ -31,12 +39,102 @@ class NavigationPageResultHydrator
     {
         $this->pageResult->setCmsPage($cmsPageEntity);
 
-        $this->setBreadcrumbs($category, $pageLoaderContext->getContext()->getSalesChannel()->getNavigationCategoryId());
+        $this->setBreadcrumbs(
+            $category,
+            $pageLoaderContext->getContext()->getSalesChannel()->getNavigationCategoryId()
+        );
 
         $this->pageResult->setResourceType($pageLoaderContext->getResourceType());
         $this->pageResult->setResourceIdentifier($pageLoaderContext->getResourceIdentifier());
 
+        $this->getAvailableFilters();
+
         return $this->pageResult;
+    }
+
+    /**
+     * @TODO: Destruct method into dedicated services and handlers
+     */
+    private function getAvailableFilters(): array
+    {
+        /**
+         * Get all listing slots
+         */
+        $listingSlots = $this->pageResult->getCmsPage()->getSections()->getBlocks()->getSlots()->filter(function($slot) {
+            /** @var $slot CmsSlotEntity */
+            return $slot->getType() === 'product-listing';
+        })->getElements();
+
+        /**
+         * Get the listing results of these slots
+         */
+        /** @var ProductListingResult[] $listings */
+        $listings = array_map(function($slot) {
+            /** @var $slot CmsSlotEntity */
+            return $slot->getData()->getListing();
+        }, $listingSlots);
+
+        /**
+         * For every listing create config from aggre
+         */
+        foreach($listings as $listing)
+        {
+            $sortings = $listing->getSortings();
+            $filters = $listing->getAggregations()->getElements();
+
+            $filters = array_map(function($aggregation) {
+                if($aggregation instanceof EntityResult) {
+                    return [
+                        'type' => 'term',
+                        'values' => array_map(function($element) {
+                            return $element->getName(); // Bad, because not all entities have names
+                        }, $aggregation->getEntities()->getElements())
+                    ];
+                }
+
+                if($aggregation instanceof StatsResult) {
+                    return [
+                        'type' => 'range',
+                        'values' => [
+                            'max' => $aggregation->getMax(),
+                            'min' => $aggregation->getMin()
+                        ]
+                    ];
+                }
+                if($aggregation instanceof TermsResult) {
+                    return [
+                        'type' => 'term',
+                        'values' => array_map(function($bucket) {
+                            /** @var $bucket Bucket */
+                            return $bucket->getKey();
+                        }, $aggregation->getBuckets())
+                    ];
+                }
+                if($aggregation instanceof MaxResult) {
+                    return [
+                        'type' => 'boolean',
+                        'max' => $aggregation->getMax()
+                    ];
+                }
+                return [get_class($aggregation)];
+            }, $filters);
+
+            $currentSorting = $listing->getSorting();
+            $currentFilters = $listing->getCurrentFilters();
+
+            $listingConfig = [
+                'sortings' => $sortings,
+                'filters' => $filters,
+                'currentSorting' => $currentSorting,
+                'currentFilters' => $currentFilters
+            ];
+
+            // WIP
+
+            $listing->addExtensions([
+                'listing_config' => $listingConfig
+            ]);
+        }
     }
 
     private function setBreadcrumbs(CategoryEntity $category, string $rootCategoryId): void
