4e0aab34038b4cd8444ad2a85bffabd6fc9e5a99
Dominic Klein <d.klein@shopware.com>
2020-01-08 23:57:03 +0100

    Poc for external checkout

--- src/VueStorefront/Controller/DecoratedCheckoutController.php
+++ src/VueStorefront/Controller/DecoratedCheckoutController.php
@@ -2,17 +2,10 @@
 
 namespace SwagVueStorefront\VueStorefront\Controller;
 
-use Shopware\Core\Checkout\Cart\SalesChannel\CartService;
-use Shopware\Core\Checkout\Order\SalesChannel\OrderService;
-use Shopware\Core\Checkout\Payment\PaymentService;
-use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
 use Shopware\Core\Framework\Routing\Annotation\RouteScope;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
-use Shopware\Storefront\Controller\CheckoutController;
-use Shopware\Storefront\Page\Checkout\Cart\CheckoutCartPageLoader;
-use Shopware\Storefront\Page\Checkout\Confirm\CheckoutConfirmPageLoader;
-use Shopware\Storefront\Page\Checkout\Finish\CheckoutFinishPageLoader;
-use Shopware\Storefront\Page\Checkout\Offcanvas\OffcanvasCartPageLoader;
+use Shopware\Storefront\Controller\StorefrontController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,14 +13,9 @@ use Symfony\Component\Routing\Annotation\Route;
 /**
  * @RouteScope(scopes={"storefront"})
  */
-class DecoratedCheckoutController extends CheckoutController
+class DecoratedCheckoutController extends StorefrontController
 {
     private $appUrl = 'https://shopware-pwa-faint-money.now.sh/';
-
-    public function __construct(CartService $cartService, CheckoutCartPageLoader $cartPageLoader, CheckoutConfirmPageLoader $confirmPageLoader, CheckoutFinishPageLoader $finishPageLoader, OrderService $orderService, PaymentService $paymentService, OffcanvasCartPageLoader $offcanvasCartPageLoader, EntityRepositoryInterface $orderRepository)
-    {
-        parent::__construct($cartService, $cartPageLoader, $confirmPageLoader, $finishPageLoader, $orderService, $paymentService, $offcanvasCartPageLoader, $orderRepository);
-    }
 
     /**
      * @Route("/checkout/finish", name="frontend.checkout.finish.page", options={"seo"="false"}, methods={"GET"})
@@ -36,11 +24,10 @@ class DecoratedCheckoutController extends CheckoutController
     {
         $orderId = $request->get('orderId');
 
-        $response = $this->redirect($this->appUrl . '?orderId=' . $orderId);
-        $response->headers->add([
+        $redirectUrl = $this->appUrl . '?orderId=' . $orderId;
+
+        return new RedirectResponse($redirectUrl, 302, [
             'sw-order-id' => $orderId
         ]);
-
-        return $response;
     }
 }
