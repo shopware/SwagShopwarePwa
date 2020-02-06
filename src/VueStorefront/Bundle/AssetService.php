6100cf57dd34139fed2caf027b32045ce2bac3e3
Dominic Klein <d.klein@shopware.com>
2020-02-06 15:44:41 +0100

    WIP: Added asset dump command

---
+++ src/VueStorefront/Bundle/AssetService.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace SwagVueStorefront\VueStorefront\Bundle;
+
+class AssetService
+{
+    public function dumpBundles(): string
+    {
+        return 'file_placeholder.zip';
+    }
+}
