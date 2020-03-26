ea3e73b9e4d71b840b60fc5a9e3dd3a265cb43a1
Dominic Klein <d.klein@shopware.com>
2020-03-26 09:45:39 +0100

    Added CmsPageController

---
+++ src/Pwa/Controller/CmsPageController.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace SwagShopwarePwa\Pwa\Controller;
+
+use Shopware\Core\Framework\Routing\Annotation\RouteScope;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * @RouteScope(scopes={"store-api"})
+ */
+class CmsPageController extends AbstractController
+{
+
+}
